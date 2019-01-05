d26a12e60dc7f9cea7c03b269bbdc74e499452e0
victornisp <46378626+victornisp@users.noreply.github.com>
2019-01-05 19:50:26 -0300

    recomenzando

--- src/App.js
+++ src/App.js
@@ -1,26 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import JumbLoginText from './components/JumbLoginText';
+import NavLoginForm from './components/NavLoginForm';
+import MainMatForm from './components/MainMatForm';
+import Prueba from './components/Prueba';
+
+import { todos } from './todos.json';
 
 class App extends Component {
+  constructor(){
+    super();
+    this.state = { todos }
+  }
+
   render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return(
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">{ todo.title.toUpperCase() } </h5>
+            <p class="card-text">{ todo.description }</p>
+            <a href="#" class="btn btn-primary">Ver pedido completo</a>
+
+          </div>
+        </div>
+        );
+    });
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+
+      {/*  vista antes de login
+        <NavLoginForm/>
+        <JumbLoginText/>
+        */}
+
+        <Prueba/>
+        <div className="container">
+          <h1>{todos.length}</h1>
+          <div className="row">
+            { todos }
+          </div>
+        </div>
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
+      
     );
   }
 }
